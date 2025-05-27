6dc96bf55d840b83289416c28a747926913b5897
Maryam <maryam.bibi@alphasquadinc.com>
2025-05-28 00:40:23 +0500

    level ui updated

--- src/Screens/Auth/Home/Profile/Level/Level.tsx
+++ src/Screens/Auth/Home/Profile/Level/Level.tsx
@@ -13,6 +13,7 @@ import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import {colors} from '../../../../../styles/colors';
 import appStyles from '../../../../../styles/styles';
 import {IMAGES} from '../../../../../assets/images';
+import {LEVELIMAGES} from '../../../../../assets/levelImages';
 
 const deviceHeight = Dimensions.get('window').height;
 const totalLevels = 20;
@@ -24,45 +25,152 @@ const dummyData = [
     icon: IMAGES.admin,
     count: 5,
     label: '1234243',
-    backgroundColor: 'red',
+    image: LEVELIMAGES.lvl5,
   },
-  {id: '2', icon: IMAGES.bdAdmin, count: 10, label: '9876543'},
-  {id: '3', icon: IMAGES.bdAdmin, count: 15, label: '9876543'},
-  {id: '4', icon: IMAGES.bdAdmin, count: 20, label: '9876543'},
-  {id: '5', icon: IMAGES.bdAdmin, count: 25, label: '9876543'},
-  {id: '6', icon: IMAGES.bdAdmin, count: 30, label: '9876543'},
-  {id: '7', icon: IMAGES.bdAdmin, count: 35, label: '9876543'},
-  {id: '8', icon: IMAGES.bdAdmin, count: 40, label: '9876543'},
-  {id: '9', icon: IMAGES.bdAdmin, count: 45, label: '9876543'},
-  {id: '10', icon: IMAGES.bdAdmin, count: 50, label: '9876543'},
-  {id: '11', icon: IMAGES.bdAdmin, count: 55, label: '9876543'},
-  {id: '12', icon: IMAGES.bdAdmin, count: 60, label: '9876543'},
-  {id: '13', icon: IMAGES.bdAdmin, count: 65, label: '9876543'},
-  {id: '15', icon: IMAGES.bdAdmin, count: 70, label: '9876543'},
-  {id: '16', icon: IMAGES.bdAdmin, count: 75, label: '9876543'},
-  {id: '17', icon: IMAGES.bdAdmin, count: 80, label: '9876543'},
-  {id: '18', icon: IMAGES.bdAdmin, count: 85, label: '9876543'},
-  {id: '19', icon: IMAGES.bdAdmin, count: 90, label: '9876543'},
-  {id: '20', icon: IMAGES.bdAdmin, count: 95, label: '987654355'},
+  {
+    id: '2',
+    icon: IMAGES.bdAdmin,
+    count: 10,
+    label: '9876543',
+    image: LEVELIMAGES.lvl10,
+  },
+  {
+    id: '3',
+    icon: IMAGES.bdAdmin,
+    count: 15,
+    label: '9876543',
+    image: LEVELIMAGES.lvl15,
+  },
+  {
+    id: '4',
+    icon: IMAGES.bdAdmin,
+    count: 20,
+    label: '9876543',
+    image: LEVELIMAGES.lvl20,
+  },
+  {
+    id: '5',
+    icon: IMAGES.bdAdmin,
+    count: 25,
+    label: '9876543',
+    image: LEVELIMAGES.lvl25,
+  },
+  {
+    id: '6',
+    icon: IMAGES.bdAdmin,
+    count: 30,
+    label: '9876543',
+    image: LEVELIMAGES.lvl30,
+  },
+  {
+    id: '7',
+    icon: IMAGES.bdAdmin,
+    count: 35,
+    label: '9876543',
+    image: LEVELIMAGES.lvl35,
+  },
+  {
+    id: '8',
+    icon: IMAGES.bdAdmin,
+    count: 40,
+    label: '9876543',
+    image: LEVELIMAGES.lvl40,
+  },
+  {
+    id: '9',
+    icon: IMAGES.bdAdmin,
+    count: 45,
+    label: '9876543',
+    image: LEVELIMAGES.lvl45,
+  },
+  {
+    id: '10',
+    icon: IMAGES.bdAdmin,
+    count: 50,
+    label: '9876543',
+    image: LEVELIMAGES.lvl50,
+  },
+  {
+    id: '11',
+    icon: IMAGES.bdAdmin,
+    count: 55,
+    label: '9876543',
+    image: LEVELIMAGES.lvl55,
+  },
+  {
+    id: '12',
+    icon: IMAGES.bdAdmin,
+    count: 60,
+    label: '9876543',
+    image: LEVELIMAGES.lvl60,
+  },
+  {
+    id: '13',
+    icon: IMAGES.bdAdmin,
+    count: 65,
+    label: '9876543',
+    image: LEVELIMAGES.lvl65,
+  },
+  {
+    id: '15',
+    icon: IMAGES.bdAdmin,
+    count: 70,
+    label: '9876543',
+    image: LEVELIMAGES.lvl70,
+  },
+  {
+    id: '16',
+    icon: IMAGES.bdAdmin,
+    count: 75,
+    label: '9876543',
+    image: LEVELIMAGES.lvl75,
+  },
+  {
+    id: '17',
+    icon: IMAGES.bdAdmin,
+    count: 80,
+    label: '9876543',
+    image: LEVELIMAGES.lvl80,
+  },
+  {
+    id: '18',
+    icon: IMAGES.bdAdmin,
+    count: 85,
+    label: '9876543',
+    image: LEVELIMAGES.lvl85,
+  },
+  {
+    id: '19',
+    icon: IMAGES.bdAdmin,
+    count: 90,
+    label: '9876543',
+    image: LEVELIMAGES.lvl90,
+  },
+  {
+    id: '20',
+    icon: IMAGES.bdAdmin,
+    count: 95,
+    label: '987654355',
+    image: LEVELIMAGES.lvl95,
+  },
+  {
+    id: '20',
+    icon: IMAGES.bdAdmin,
+    count: 100,
+    label: '987654355',
+    image: LEVELIMAGES.lvl100,
+  },
 ];
 
 export default function Level({navigation}) {
   const renderItem = ({item}) => (
     <View style={styles.itemContainer}>
-      <View
-        style={[
-          styles.iconContainer,
-          {
-            backgroundColor: item.backgroundColor
-              ? item?.backgroundColor
-              : null,
-          },
-        ]}>
+      <ImageBackground style={styles.iconContainer} source={item.image}>
         <View style={styles.imageWrapper}>
           <Image source={item.icon} style={styles.iconImage} />
         </View>
         <Text style={styles.countText}>{item.count}</Text>
-      </View>
+      </ImageBackground>
       <View style={styles.labelWrapper}>
         <Text numberOfLines={1} style={styles.labelText}>
           {item.label}
@@ -91,7 +199,9 @@ export default function Level({navigation}) {
           <View style={styles.progressRow}>
             <Text style={styles.levelLabel}>Lv {achievedLevels}</Text>
             <View style={styles.progressBar}>
-              <View style={[styles.achievedBar, { width: `${progressPercentage}%` }]} />
+              <View
+                style={[styles.achievedBar, {width: `${progressPercentage}%`}]}
+              />
             </View>
             <Text style={styles.levelLabel}>Lv {totalLevels}</Text>
           </View>
@@ -171,14 +281,14 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '700',
     color: colors.white,
-    textAlign:"center"
+    textAlign: 'center',
   },
   itemContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     borderRadius: 20,
-    padding:5,
+    padding: 5,
   },
   iconContainer: {
     flexDirection: 'row',
@@ -186,7 +296,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: colors.white,
     borderRadius: 30,
-    width: deviceHeight * 0.1,justifyContent:"space-between"
+    width: deviceHeight * 0.1,
+    justifyContent: 'space-between',overflow:"hidden"
   },
   imageWrapper: {
     height: 25,
@@ -211,7 +322,7 @@ const styles = StyleSheet.create({
     maxWidth: deviceHeight * 0.13,
   },
   labelText: {
-   ...appStyles.headline2,
+    ...appStyles.headline2,
     color: colors.white,
     textAlign: 'left',
   },
